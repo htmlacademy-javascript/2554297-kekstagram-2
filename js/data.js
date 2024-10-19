1d40eeac0f56c619d7ec55f16662b9e1355cf76e
aLesya66 <alesyay4@gmail.com>
2024-10-19 17:06:49 +0300

    добавлен новый модуль mas.js

--- js/data.js
+++ js/data.js
@@ -1,8 +1,7 @@
-import {getRandomArrayElement, getRandomInteger} from './rand.js';
-
 const description = ['пляж', 'лес', 'море', 'юг', 'оеан', 'лето', 'весна', 'котики', 'будни',
   'работа', 'лайфхаки', 'блог', 'прайс-лист', 'ночь', 'эстетика',
-  'книги', 'еда', 'зоопарк', 'театр', 'прогулка', 'путешествие', 'парк', 'закат', 'музыка', 'хобби' ];
+  'книги', 'еда', 'зоопарк', 'театр', 'прогулка', 'путешествие', 'парк', 'закат', 'музыка', 'хобби',
+];
 const messageCommentator = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -18,20 +17,4 @@ const nameCommentator = [
 
 const count = 25;
 
-const createComment = (indexx) => ({
-  id: indexx + 1,
-  avatar: `img/avatar-${ getRandomInteger(1, 6) } .svg`,
-  message: `${getRandomArrayElement(messageCommentator)}`,
-  name: `${getRandomArrayElement(nameCommentator)}`,
-});
-
-const createUsers = (index) => ({
-  id: index + 1,
-  url: `photos/${ index + 1 }.jpg` ,
-  description: `${getRandomArrayElement(description)}`,
-  likes: getRandomInteger(1, 200),
-  comments: Array.from({ length: getRandomInteger(1, 30) }, (__, indexx) => createComment(indexx)),
-});
-const finalMas = Array.from({length:count}, (__, index) => createUsers(index));
-
-export {finalMas};
+export {description, messageCommentator, nameCommentator, count};
--- js/rand.js
+++ js/rand.js
@@ -4,7 +4,6 @@ const getRandomInteger = (a, b) => {
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
-
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 export {getRandomArrayElement, getRandomInteger };
