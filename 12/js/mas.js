125f7298e0725032314da3f4bae3bdad9169a06b
Keks <keks@htmlacademy.ru>
2024-12-09 21:52:42 +0300

    :heavy_check_mark: Сборка #12

--- 12/js/mas.js
+++ 12/js/mas.js
@@ -17,4 +17,27 @@ const createUsers = (index) => ({
 });
 const finalMas = () => Array.from({length:count}, (__, index) => createUsers(index));
 
-export {finalMas, createUsers, createComment};
+const makeElement = (name, className, message) => {
+  const element = document.createElement(name);
+  element.classList.add(className);
+  if (message) {
+    element.textContent = message;
+  }
+  return element;
+};
+
+const createSocialComment = (mas, container) => {
+  mas.forEach((element) => {
+    const listElement = makeElement('li', 'social__comment');
+    const picture = makeElement('img', 'social__picture');
+    picture.src = element.avatar;
+    picture.alt = element.name;
+    listElement.appendChild(picture);
+    const commentText = makeElement('p', 'social__text', element.message);
+    listElement.appendChild(commentText);
+    container.appendChild(listElement);
+  });
+};
+
+export {finalMas, createUsers, createComment, createSocialComment};
+
--- 12/js/rand.js
+++ 12/js/rand.js
@@ -9,4 +9,4 @@ const getRandomArrayElement = (elements) => elements[getRandomInteger(0, element
 const isEscapeKey = (evt) => evt.key === 'Escape';
 const isEnterKey = (evt) => evt.key === 'Enter';
 
-export {getRandomArrayElement, getRandomInteger, isEscapeKey, isEnterKey };
+export {getRandomArrayElement, getRandomInteger, isEscapeKey, isEnterKey};
